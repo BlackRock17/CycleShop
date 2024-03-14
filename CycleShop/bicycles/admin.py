1a2166bd53f55d4f436720737469e7cc10770fa4
Lyubomir <119952676+BlackRock17@users.noreply.github.com>
2024-03-15 00:33:50 +0200

    admin site, filters and searches

--- CycleShop/bicycles/admin.py
+++ CycleShop/bicycles/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
+from django.contrib.contenttypes.models import ContentType
+
 from .models import (
     MountainBicycle, RoadBicycle, ElectricBicycle, BicycleInventory, Bicycle
 )
+
+
+class BicycleTypeFilter(admin.SimpleListFilter):
+    title = 'Bicycle Type'
+    parameter_name = 'bicycle_type'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('mountainbicycle', 'Mountain Bicycle'),
+            ('roadbicycle', 'Road Bicycle'),
+            ('electricbicycle', 'Electric Bicycle')
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'mountainbicycle':
+            return queryset.filter(bicycle__mountainbicycle__isnull=False)
+        elif self.value() == 'roadbicycle':
+            return queryset.filter(bicycle__roadbicycle__isnull=False)
+        elif self.value() == "electricbicycle":
+            return queryset.filter(bicycle__electricbicycle__isnull=False)
 
 
 class BicycleInventoryInline(admin.TabularInline):
@@ -11,28 +33,35 @@ class BicycleInventoryInline(admin.TabularInline):
 
 @admin.register(MountainBicycle)
 class MountainBicycleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
     inlines = [BicycleInventoryInline]
 
 
 @admin.register(RoadBicycle)
 class RoadBicycleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
     inlines = [BicycleInventoryInline]
 
 
 @admin.register(ElectricBicycle)
 class ElectricBicycleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'category')
+    inlines = [BicycleInventoryInline]
 
 
 @admin.register(BicycleInventory)
 class BicycleInventoryAdmin(admin.ModelAdmin):
-    list_display = ('bicycle', 'size', 'quantity')
+    list_display = ('bicycle', 'category', 'size', 'quantity')
     list_editable = ('quantity',)
-    list_filter = ('size', 'quantity')
+    list_filter = (BicycleTypeFilter, 'size',)
+    search_fields = ('bicycle__name',)
 
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        qs = qs.filter(quantity__gt=0)
-        return qs
-
+    def category(self, obj):
+        if hasattr(obj.bicycle, 'mountainbicycle'):
+            return obj.bicycle.mountainbicycle.category
+        elif hasattr(obj.bicycle, 'roadbicycle'):
+            return obj.bicycle.roadbicycle.category
+        elif hasattr(obj.bicycle, 'electricbicycle'):
+            return obj.bicycle.electricbicycle.category
+        return '-'
 
