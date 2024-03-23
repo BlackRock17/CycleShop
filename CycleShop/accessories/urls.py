827eb9e532744d95dd6aa4d07cc49822438ba9f1
Lyubomir <119952676+BlackRock17@users.noreply.github.com>
2024-03-23 14:27:53 +0200

    Accessories Details View and Template

--- CycleShop/accessories/urls.py
+++ CycleShop/accessories/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from CycleShop.accessories.views import AccessoriesListView
+from CycleShop.accessories.views import AccessoriesListView, AccessoryDetailView
 
 urlpatterns = (
+    path('accessories/<int:pk>/', AccessoryDetailView.as_view(), name='accessory_detail'),
     path('accessories/', AccessoriesListView.as_view(), name='accessories_list'),
     path('accessories/<str:category>/', AccessoriesListView.as_view(), name='accessories_list_by_category'),
+
 )
--- CycleShop/accessories/views.py
+++ CycleShop/accessories/views.py
@@ -1,10 +1,5 @@
 from django.views import generic as views
-from CycleShop.accessories.forms import AccessoryCategoryForm
-from CycleShop.accessories.models import Accessories, Category
-
-
-from django.views import generic as views
-from .models import Accessories, Category
+from .models import Accessories
 from .forms import AccessoryCategoryForm
 
 
@@ -13,9 +8,9 @@ class AccessoriesListView(views.ListView):
     template_name = 'accessories/accessories_list.html'
     context_object_name = 'accessories'
 
-    def get_queryset(self):
+    def get_queryset(self, category=None):
         queryset = super().get_queryset()
-        category = self.kwargs.get('category')
+        category = category or self.kwargs.get('category')
         form = AccessoryCategoryForm(self.request.GET)
         if form.is_valid():
             selected_category = form.cleaned_data['category']
@@ -31,4 +26,23 @@ class AccessoriesListView(views.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['form'] = AccessoryCategoryForm(self.request.GET)
+        accessories = context['accessories']
+
+        for accessory in accessories:
+            accessory.pk_type = type(accessory.pk).__name__
+        return context
+
+
+class AccessoryDetailView(views.DetailView):
+    model = Accessories
+    template_name = 'accessories/accessory_detail.html'
+    context_object_name = 'accessory'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        accessory = self.get_object()
+        context['accessory_fields'] = [
+            {'name': field.name, 'verbose_name': field.verbose_name, 'value': getattr(accessory, field.name)}
+            for field in accessory._meta.fields if field.verbose_name not in ("ID", "quantity")
+        ]
         return context
